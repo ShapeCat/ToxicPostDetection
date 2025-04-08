e0c505093318ee0f1d3c1e69882aca0fcc563218
ShapeCat <rs0303@mail.ru>
2025-04-08 03:29:08 +0300

    fix typo

--- src/utils/environment.py
+++ src/utils/environment.py
@@ -6,5 +6,5 @@ def load_env(value_name:str, default_value:Optional[Any]=None) -> Any:
         env = dotenv_values("../.env")
         return env.get(value_name, default_value)
     except Exception as e:
-        print(f"There is an error while reading .emv file: {e}")
+        print(f"There is an error while reading .env file: {e}")
         return default_value
