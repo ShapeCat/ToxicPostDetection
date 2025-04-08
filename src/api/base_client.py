d2d3430dd46df0e01ec9bb0fc7971268658f2cd8
ShapeCat <rs0303@mail.ru>
2025-04-08 07:16:29 +0300

    Add base vk api client

---
+++ src/api/base_client.py
@@ -0,0 +1,41 @@
+import requests
+from typing import Dict, Any
+from .exceptions import VKAPIError
+
+
+class BaseAPIClient:
+    url = 'https://api.vk.com/method/'
+    api_version = 5.199
+
+    def __init__(self, access_token):
+        self.access_token = access_token     
+
+    def post_request(self, method:str, params:Dict[str, str]) -> Dict[str, str]:
+        params.update({
+            'access_token': self.access_token,
+            'v': self.api_version
+        })
+        
+        response = requests.post(self.url+method, params=params)
+        response.raise_for_status()
+        json_response = response.json()
+        
+        if 'error' in json_response:
+            error = json_response['error']
+            raise VKAPIError(f"There is an error while calling VK API {method} method: {error['error_msg']}({error['error_code']})")          
+        return json_response.get('response')
+    
+    def get_request(self, method:str, params:Dict[str, str]) -> Dict[str, str]:
+        params.update({
+            'access_token': self.access_token,
+            'v': self.api_version
+        })
+        
+        response = requests.get(self.url+method, params=params)
+        response.raise_for_status()
+        json_response = response.json()
+        
+        if 'error' in json_response:
+            error = json_response['error']
+            raise VKAPIError(f"There is an error while calling VK API {method} method: {error['error_msg']}({error['error_code']})")               
+        return json_response.get('response')
---
+++ src/api/exceptions.py
@@ -0,0 +1,2 @@
+class VKAPIError(Exception):
+    pass
