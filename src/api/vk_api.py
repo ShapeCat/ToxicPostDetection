7050f2661a1b5cd2ce126228d36fa76f6a075907
ShapeCat <rs0303@mail.ru>
2025-04-09 10:29:26 +0300

    Add vk api interface

---
+++ src/api/vk_api.py
@@ -0,0 +1,10 @@
+
+from .wall_client import WallAPIClient
+from .chat_client import ChatClient
+
+
+class VK_API:
+    def __init__(self, access_token:str, community_token:str, service_key:str, admin_id:int):
+        self.wall = WallAPIClient(access_token, community_token, service_key)
+        self.chat = ChatClient(access_token, community_token, service_key, admin_id)
+    
