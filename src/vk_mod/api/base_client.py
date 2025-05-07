9932f4513d2c58493bdbffc8e3ed8d66d0a33ba3
ShapeCat <rs0303@mail.ru>
2025-05-08 02:41:30 +0300

    Add docstring for base api client

--- src/vk_mod/api/base_client.py
+++ src/vk_mod/api/base_client.py
@@ -7,11 +7,33 @@ class BaseAPIClient:
     api_version = 5.199
 
     def __init__(self, access_token:str, community_token:str, service_key:str) -> None:
+        """
+        Initialize a BaseAPIClient instance.
+
+        Args:
+            access_token (str): The access token for the VK API.
+            community_token (str): The community token for the VK API.
+            service_key (str): The service key for the VK API.
+        """
         self.access_token = access_token 
         self.community_token = community_token
         self.service_key = service_key
 
     def post_request(self, method:str, params:dict[str], access_token:str|None=None) -> dict[str, str]:
+        """
+        Make a POST request to VK API method.
+
+        Args:
+            method (str): VK API method name.
+            params (dict[str]): Parameters for the request.
+            access_token (str|None): The access token to use for the request. If None, the default access token is used.
+
+        Returns:
+            dict[str, str]: The response from the VK API.
+
+        Raises:
+            VKAPIError: If the VK API returns an error.
+        """
         params.update({
             'access_token': access_token if access_token  else self.access_token,
             'v': self.api_version
@@ -27,6 +49,20 @@ class BaseAPIClient:
         return json_response.get('response')
     
     def get_request(self, method:str, params:dict[str, str], access_token:str|None=None) -> dict[str, str]:
+        """
+        Make a GET request to VK API method.
+
+        Args:
+            method (str): VK API method name.
+            params (dict[str]): Parameters for the request.
+            access_token (str|None): The access token to use for the request. If None, the default access token is used.
+
+        Returns:
+            dict[str, str]: The response from the VK API.
+
+        Raises:
+            VKAPIError: If the VK API returns an error.
+        """
         params.update({
             'access_token': access_token if access_token  else self.access_token,
             'v': self.api_version
