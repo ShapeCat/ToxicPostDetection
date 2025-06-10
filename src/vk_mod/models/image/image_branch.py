e77959214b16bd6015ff5436d3aa0070c98625da
ShapeCat <rs0303@mail.ru>
2025-06-10 08:24:27 +0300

    Remove dostring override on image branch

--- src/vk_mod/models/image/image_branch.py
+++ src/vk_mod/models/image/image_branch.py
@@ -4,12 +4,6 @@ from keras import layers, Model, saving, applications
 @tf.keras.utils.register_keras_serializable()
 class ImageBranch(Model):
     def __init__(self, **kwargs) -> None:
-        """
-        Initialize the ImageBranch.
-
-        Args:
-            **kwargs: Additional keyword arguments for the base Model initialization.
-        """
         super().__init__(**kwargs)
         self.base_model = applications.EfficientNetB0(
             include_top=False,
@@ -21,38 +15,14 @@ class ImageBranch(Model):
         self.dense:layers.Dense = layers.Dense(128, activation='relu')
         self.dropout:layers.Dropout = layers.Dropout(0.3)
 
-    def call(self, inputs:tf.Tensor) -> tf.Tensor:
-        """
-        Forward pass of the model.
-
-        Args:
-            inputs (tf.Tensor): Input tensor with shape (batch_size, 224, 224, 3).
-
-        Returns:
-            tf.Tensor: Output tensor after passing through the model layers.
-        """
+    def call(self, inputs, training=None, mask=None):
         x:tf.Tensor = self.base_model(inputs)
         x:tf.Tensor = self.pooling2d(x)
         return self.dropout(self.dense(x))
     
     def get_config(self) -> dict:
-        """
-        Get the configuration of the model as a dictionary.
-
-        Returns:
-            dict: The configuration of the model.
-        """
         return super().get_config()
     
     @classmethod
-    def from_config(cls, config:dict) -> 'ImageBranch':
-        """
-        Initialize the `ImageModel` from a configuration dictionary.
-
-        Args:
-            config (dict): A dictionary containing the configuration for the model.
-
-        Returns:
-            ImageBranch: An instance of `ImageBranch` with the specified configuration.
-        """
+    def from_config(cls, config, custom_objects=None):
         return cls(**config)
