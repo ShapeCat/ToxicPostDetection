4a32631a75e07b5ae1b99097202915564ed1e007
ShapeCat <rs0303@mail.ru>
2025-06-18 19:18:46 +0300

    Rename wall api client methods to short one

--- src/vk_mod/api/wall_client.py
+++ src/vk_mod/api/wall_client.py
@@ -1,25 +1,40 @@
 from .base_client import BaseAPIClient
-
+from typing import Literal
 
 class WallClient(BaseAPIClient):
-    def __init__(self, community_token:str, access_token:str,) -> None:
+    def __init__(self,
+                 community_token: str,
+                 access_token: str
+                 ) -> None:
         super().__init__(community_token)
         self.access_token = access_token
 
-    def get_wall_posts(self, community_id:int|str, count=100) -> list[dict[str, str]]:
+    def get_all(self,
+                community_id: str,
+                count: int=100,
+                filter: Literal["suggests", "postponed", "owner", "others", "all", "donut"]="all"
+                ) -> list[dict[str, str]]:      
         params = {
             "domain": f"-{community_id}", 
             "count": count,
+            "filter": filter
         }
         return self.get_request("wall.get", params, self.access_token)["items"]
 
-    def get_post_by_id(self, post_id:int|str, community_id:int|str) -> dict[str, str]:
+    def get_by_id(self,
+                  community_id: str,
+                  post_id: str
+                  ) -> dict[str, str]:
         params = {
             "posts": f"-{community_id}_{post_id}" 
         }
         return self.get_request("wall.getById", params, self.access_token)["items"][0]
 
-    def get_post_comments(self, post_id:int, community_id:int, count:int=100) -> list[dict[str, str]]:
+    def get_comments(self,
+                     community_id: int,
+                     post_id: int, 
+                     count: int=100
+                     ) -> list[dict[str, str]]:
         params = {
             "owner_id": f"-{community_id}",
             "post_id": post_id,
@@ -27,6 +42,9 @@ class WallClient(BaseAPIClient):
         }
         return self.get_request("wall.getComments", params, self.access_token)["items"]
 
-    def get_new_posts_since(self, community_id:int, since_timestamp:int) -> list[dict[str, str]]:
-        posts = self.get_wall_posts(community_id)
+    def get_all_since(self,
+                      community_id:int,
+                      since_timestamp:int
+                      ) -> list[dict[str, str]]:
+        posts = self.get_all(community_id)
         return [post for post in posts if post["date"]>since_timestamp]
