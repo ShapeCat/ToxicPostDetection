48f722db248644ee21d703d475076481fd7e0b79
ShapeCat <rs0303@mail.ru>
2025-06-19 10:03:58 +0300

    Clean download image module

--- src/vk_mod/data/download.py
+++ src/vk_mod/data/download.py
@@ -1,31 +1,21 @@
 import os, requests
 from PIL import Image
 from io import BytesIO
-from pathlib import Path
 from pandas import DataFrame
 from datasets import load_dataset
 
 
-default_path = Path("../data/raw/")
 Image.MAX_IMAGE_PIXELS = None
 
 
 # TODO Add check if file exists
 # TODO Add check for non-existant save folder
-def load_from_huggingface(dataset_name:str, split="train", target_path:str|Path=default_path) -> None:
-    """
-    Downloads a dataset from the huggingface datasets repository and saves it as a csv file to the target_path.
-    
-    Args:
-        dataset_name (str): The name of the dataset on the huggingface datasets repository.
-        split (str): The split of the dataset to download. Defaults to "train".
-        target_path (str|Path): The path to save the dataset to. Defaults to the default_path.
-    """
-    save_path = Path(target_path, dataset_name.split('/')[-1]).with_suffix(".csv")
-    if isinstance(target_path, str):
-        target_path = Path(target_path)
+def load_from_hugginface(dataset_name: str,
+                         target_path: os.PathLike,
+                         split: str="train"
+                         ) -> None:
     dataset = load_dataset(dataset_name, split=split)
-    DataFrame(dataset).to_csv(save_path)
+    DataFrame(dataset).to_csv(target_path)
 
 
 def download_image(uri: str,
@@ -36,4 +26,4 @@ def download_image(uri: str,
     image = Image.open(BytesIO(response.content))
     if image.mode != mode:
         image = image.convert(mode)
-    image.save(target_path)  
+    image.save(target_path)
