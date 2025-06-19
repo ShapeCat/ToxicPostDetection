019a20b68a7636a401ad818490a6383bcb7e8082
ShapeCat <rs0303@mail.ru>
2025-06-19 09:28:29 +0300

    Add download image to download module

--- src/vk_mod/data/download.py
+++ src/vk_mod/data/download.py
@@ -1,9 +1,13 @@
+import os, requests
+from PIL import Image
+from io import BytesIO
 from pathlib import Path
 from pandas import DataFrame
 from datasets import load_dataset
 
 
 default_path = Path("../data/raw/")
+Image.MAX_IMAGE_PIXELS = None
 
 
 # TODO Add check if file exists
@@ -22,3 +26,14 @@ def load_from_huggingface(dataset_name:str, split="train", target_path:str|Path=
         target_path = Path(target_path)
     dataset = load_dataset(dataset_name, split=split)
     DataFrame(dataset).to_csv(save_path)
+
+
+def download_image(uri: str,
+                   target_path: os.PathLike,
+                   mode: str = "RGB"
+                   ) -> None:
+    response = requests.get(uri , timeout=3)
+    image = Image.open(BytesIO(response.content))
+    if image.mode != mode:
+        image = image.convert(mode)
+    image.save(target_path)  
