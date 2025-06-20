4e71e614d2e6545af9580773fe5b258a57212109
ShapeCat <rs0303@mail.ru>
2025-06-21 02:44:13 +0300

    Refactor naming in enviroment tests

--- src/vk_mod/utils/enviroment_test.py
+++ src/vk_mod/utils/enviroment_test.py
@@ -1,5 +1,6 @@
 import pytest
-from environment import load_config, NoOptionError, NoSectionError
+from pathlib import Path
+from .environment import load_config, NoOptionError, NoSectionError
 
 
 TEST_CONFIG = """
@@ -12,7 +13,7 @@ bool_value = true
 
 
 @pytest.fixture
-def temp_config(tmp_path):
+def config_fixture(tmp_path):
     def generate_ini_file(content):
         ini_file_path = tmp_path / "config.ini"
         ini_file_path.write_text(content)
@@ -24,8 +25,8 @@ def temp_config(tmp_path):
     ("str", "Строка"),
     ("bool", True),
 ])
-def test_load_str_value_exists(tmp_config, return_type, expected):
-    ini_path = tmp_config(TEST_CONFIG)
+def test_load_str_value_success(config_fixture, return_type, expected):
+    ini_path = config_fixture(TEST_CONFIG)
     assert load_config("section", "str_value", file_path=ini_path, return_type=return_type) == expected
 
 
@@ -33,8 +34,8 @@ def test_load_str_value_exists(tmp_config, return_type, expected):
     ("bool", True),
     ("str", "true"),
 ])
-def test_load_bool_value_exists(tmp_config, return_type, expected):
-    ini_path = tmp_config(TEST_CONFIG)
+def test_load_bool_value_success(config_fixture, return_type, expected):
+    ini_path = config_fixture(TEST_CONFIG)
     assert load_config("section", "bool_value", file_path=ini_path, return_type=return_type) == expected
 
 
@@ -43,8 +44,8 @@ def test_load_bool_value_exists(tmp_config, return_type, expected):
     ("str", "4"), 
     ("bool", True), 
 ])
-def test_load_int_value_exists(tmp_config, return_type, expected):
-    ini_path = tmp_config(TEST_CONFIG)
+def test_load_int_value_success(config_fixture, return_type, expected):
+    ini_path = config_fixture(TEST_CONFIG)
     assert load_config("section", "int_value", file_path=ini_path, return_type=return_type) == expected
 
 
@@ -53,26 +54,27 @@ def test_load_int_value_exists(tmp_config, return_type, expected):
     ("str", "4.44"),
     ("bool", True),
 ])
-def test_load_float_value_exists(tmp_config, return_type, expected):
-    ini_path = tmp_config(TEST_CONFIG)
+def test_load_float_value_success(config_fixture, return_type, expected):
+    ini_path = config_fixture(TEST_CONFIG)
     assert load_config("section", "float_value", file_path=ini_path, return_type=return_type) == expected
 
 
 def test_file_not_found():
+    config_path = Path("missing.ini")
     with pytest.raises(FileNotFoundError):
-        load_config("section", "str_value", file_path="missing.ini")
-    assert load_config("section", "str_value", file_path="missing.ini", default_value="default") == "default"
+        load_config("section", "str_value", file_path=config_path)
+    assert load_config("section", "str_value", file_path=config_path, default_value="default") == "default"
 
 
-def test_missing_section(tmp_config):
-    ini_path = tmp_config(TEST_CONFIG)
+def test_missing_section(config_fixture):
+    ini_path = config_fixture(TEST_CONFIG)
     with pytest.raises(NoSectionError):
         load_config("missing_section", "str_value", file_path=ini_path)
     assert load_config("missing_section", "str_value", file_path=ini_path, default_value="default") == "default"
 
 
-def test_missing_key(tmp_config):
-    ini_path = tmp_config(TEST_CONFIG)
+def test_missing_key(config_fixture):
+    ini_path = config_fixture(TEST_CONFIG)
     with pytest.raises(NoOptionError):
         load_config("section", "missing_key", file_path=ini_path)
     assert load_config("section", "missing_key", file_path=ini_path, default_value="default") == "default"
@@ -82,8 +84,8 @@ def test_missing_key(tmp_config):
     ("str_value", "int"), 
     ("str_value", 'float'),
 ])
-def test_conversion_error(tmp_config, key, return_type):
-    ini_path = tmp_config(TEST_CONFIG)
+def test_conversion_error(config_fixture, key, return_type):
+    ini_path = config_fixture(TEST_CONFIG)
     with pytest.raises(ValueError):
         load_config("section", key, file_path=ini_path, return_type=return_type)
     assert load_config("section", key, file_path=ini_path, return_type=return_type, default_value="default") == "default"
