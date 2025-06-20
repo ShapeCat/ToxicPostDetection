80046182addb5f8f43dee0f4b1bebedd78dfa099
ShapeCat <rs0303@mail.ru>
2025-06-20 13:43:44 +0300

    Update tests for new chat client

--- src/vk_mod/api/chat_client_test.py
+++ src/vk_mod/api/chat_client_test.py
@@ -25,13 +25,15 @@ def test_send_message_success(chat_client, requests_mock):
     THEN:  the response is parsed correctly, and the URL contains the correct parameters
     """
     user_id = 12345
+    message = "Test message"
     expected_response = {"response": 1}
     requests_mock.post(f"{BaseAPIClient.url}messages.send", json=expected_response)
     
-    result = chat_client.send("Test message", user_id)
+    result = chat_client.send(user_id, message)
+
     assert result == expected_response["response"]
     assert f"peer_id={user_id}" in requests_mock.last_request.url
-    assert "message=Test+message" in requests_mock.last_request.url
+    assert f"message={'+'.join(message.split())}" in requests_mock.last_request.url
     assert "random_id" in requests_mock.last_request.url
 
 
@@ -44,6 +46,7 @@ def test_get_message_by_id_success(chat_client, requests_mock):
     THEN:  the response is parsed correctly, and the URL contains the correct parameters
     """
     user_id = 12345
+    message_id = 1
     expected_response = {
         "response": 
         {"items": [
@@ -53,10 +56,12 @@ def test_get_message_by_id_success(chat_client, requests_mock):
     }
     requests_mock.get(f"{BaseAPIClient.url}messages.getById", json=expected_response)
     
-    result = chat_client.get_by_id(1, user_id)
-    assert isinstance(result, list) 
-    assert result[0]["id"] == 1
+    result = chat_client.get_by_id(user_id, message_id)
+
+    assert isinstance(result, dict) 
+    assert result["id"] == 1
     assert f"peer_id={user_id}" in requests_mock.last_request.url
+    assert f"message_ids={message_id}" in requests_mock.last_request.url
 
 
 def test_get_all_chat_messages_success(chat_client, requests_mock):
@@ -80,6 +85,7 @@ def test_get_all_chat_messages_success(chat_client, requests_mock):
     requests_mock.get(f"{BaseAPIClient.url}messages.getHistory", json=expected_response)
     
     result = chat_client.get_all(user_id, count=count)
+
     assert len(result) == 2 
     assert f"peer_id={user_id}" in requests_mock.last_request.url
     assert f"count={count}" in requests_mock.last_request.url
@@ -95,16 +101,16 @@ def test_delete_message_parametrize_success(chat_client, requests_mock, delete_f
     THEN:  the response is parsed correctly, and the URL contains the correct parameters
     """
     user_id = 12345
-    delete_for_all = 1 if delete_for_all else 0
-    cmids = 1
+    message_id = 1
     excpected_response = {"response": 1}
     requests_mock.post(f"{BaseAPIClient.url}messages.delete", json=excpected_response)
     
-    response = chat_client.delete(user_id, cmids, delete_for_all=delete_for_all)
-    assert response == 1
-    assert f"delete_for_all={delete_for_all}" in requests_mock.last_request.url
+    response = chat_client.delete(user_id, message_id, delete_for_all=delete_for_all)
+
+    assert response
+    assert f"delete_for_all={1 if delete_for_all else 0}" in requests_mock.last_request.url
     assert f"peer_id={user_id}" in requests_mock.last_request.url
-    assert f"cmids={cmids}" in requests_mock.last_request.url
+    assert f"message_id={message_id}" in requests_mock.last_request.url
 
 
 def test_edit_message_success(chat_client, requests_mock):
@@ -116,13 +122,14 @@ def test_edit_message_success(chat_client, requests_mock):
     THEN:  the response is parsed correctly, and the URL contains the correct parameters
     """
     user_id = 12345
-    cmids = 1
+    message_id = 1
     mew_text = "New text"
     expected_response = {"response": 1}
     requests_mock.post(f"{BaseAPIClient.url}messages.edit", json=expected_response)
     
-    result = chat_client.edit(user_id, cmids, mew_text)
-    assert result == 1 
+    result = chat_client.edit(user_id, message_id, mew_text)
+    
+    assert result 
     assert f"peer_id={user_id}" in requests_mock.last_request.url
-    assert f"cmid={cmids}" in requests_mock.last_request.url
+    assert f"cmid={message_id}" in requests_mock.last_request.url
     assert f"message={mew_text.replace(' ', '+')}" in requests_mock.last_request.url
