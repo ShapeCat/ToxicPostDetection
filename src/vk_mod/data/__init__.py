1aaf4808ab2cdc109216444eb80e8b3725f7273e
ShapeCat <rs0303@mail.ru>
2025-06-20 11:57:26 +0300

    Fix missing download image in data init file

--- src/vk_mod/data/__init__.py
+++ src/vk_mod/data/__init__.py
@@ -1,3 +1,3 @@
 from .loader import load_dataset
-from .download import load_from_huggingface
+from .download import load_from_hugginface, download_image
 from .dataset_generator import DatasetGenerator
