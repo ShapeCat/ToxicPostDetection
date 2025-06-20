d736dace98f9edb97abc12182ee0a84a340c15a0
ShapeCat <rs0303@mail.ru>
2025-06-20 12:05:35 +0300

    Update docstring in base api client

--- src/vk_mod/api/base_client.py
+++ src/vk_mod/api/base_client.py
@@ -9,62 +9,60 @@ class BaseAPIClient:
 
     def __init__(self, community_token: str) -> None:
         """
-        Initialize a BaseAPIClient instance.
+        Initialize a VK API client with a community token.
 
         Args:
-            access_token (str): The access token for the VK API.
-            community_token (str): The community token for the VK API.
-            service_key (str): The service key for the VK API.
+            community_token (str): The community token to use for the client.
         """
         self.community_token = community_token
 
     def post_request(self,
                      method: str,
                      params: dict[str, str] = {},
-                     access_token: str|None = None
+                     access_token: str | None = None
                      ) -> Any:
         """
         Make a POST request to VK API method.
 
         Args:
             method (str): VK API method name.
-            params (dict[str]): Parameters for the request.
-            access_token (str|None): The access token to use for the request. If None, the default access token is used.
+            params (dict[str, str]): Parameters for the request.
+            access_token (str | None): The access token to use for the request. If None, the community token is used.
 
         Returns:
-            dict[str, str]: The response from the VK API.
+            Any: The response from the VK API.
 
         Raises:
             VKAPIError: If the VK API returns an error.
         """
         params.update({
-            'access_token': access_token if access_token  else self.community_token,
+            'access_token': access_token if access_token else self.community_token,
             'v': self.api_version
-        })
-        
-        response = requests.post(self.url+method, params=params)
+        })       
+        response = requests.post(self.url + method, params=params)
         response.raise_for_status()
         json_response = response.json()
         
         if 'error' in json_response:
             error = json_response['error']
-            raise VKAPIError(f"There is an error while calling VK API {method} method: {error['error_msg']}({error['error_code']})")          
+            raise VKAPIError(f"There is an error while calling VK API {method} method: {error['error_msg']}({error['error_code']})")
         return json_response.get('response')
     
     def get_request(self,
                     method: str,
                     params: dict[str, str] = {},
-                    access_token: str|None = None) -> Any:
+                    access_token: str | None = None
+                    ) -> Any:
         """
         Make a GET request to VK API method.
 
         Args:
             method (str): VK API method name.
-            params (dict[str]): Parameters for the request.
-            access_token (str|None): The access token to use for the request. If None, the default access token is used.
+            params (dict[str, str]): Parameters for the request.
+            access_token (str | None): The access token to use for the request. If None, the community token is used.
 
         Returns:
-            dict[str, str]: The response from the VK API.
+            Any: The response from the VK API.
 
         Raises:
             VKAPIError: If the VK API returns an error.
@@ -72,8 +70,7 @@ class BaseAPIClient:
         params.update({
             'access_token': access_token if access_token  else self.community_token,
             'v': self.api_version
-        })
-        
+        })       
         response = requests.get(self.url+method, params=params)
         response.raise_for_status()
         json_response = response.json()
@@ -82,3 +79,4 @@ class BaseAPIClient:
             error = json_response['error']
             raise VKAPIError(f"There is an error while calling VK API {method} method: {error['error_msg']}({error['error_code']})")               
         return json_response.get('response')
+
