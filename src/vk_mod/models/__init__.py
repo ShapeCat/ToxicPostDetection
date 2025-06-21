ea16a647fc018597e7205a2e3ee62944d620fc5c
ShapeCat <rs0303@mail.ru>
2025-06-21 23:27:17 +0300

    Rename toxic classifier to post classifier

--- src/vk_mod/models/__init__.py
+++ src/vk_mod/models/__init__.py
@@ -1,1 +1,1 @@
-from .toxic_classificator import build_model, build_and_train, predict_from_file, predict_from_url, evaluate_data_combinations, load_model
+from .post_classifier import build_model, build_and_train, predict_from_file, predict_from_url, evaluate_data_combinations, load_model
