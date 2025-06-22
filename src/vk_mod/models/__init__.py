014109feaad154bfd61cf7cdadf78b9244b1fa16
ShapeCat <rs0303@mail.ru>
2025-06-22 08:09:26 +0300

    Update models init file

--- src/vk_mod/models/__init__.py
+++ src/vk_mod/models/__init__.py
@@ -1,1 +1,1 @@
-from .post_classifier import build_model, build_and_train, predict_from_file, predict_from_url, evaluate_data_combinations, load_model
+from .post_classifier import build_model, train_model, predict_from_file, predict_from_url, evaluate_data_combinations, load_model
